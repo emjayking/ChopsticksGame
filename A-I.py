f03e4cd16f0f4649c9d72beca449b51b3a19f750
emjayking <emjayking23@hotmail.co.nz>
2018-10-31 10:16:21 +1300

    Update A-I.py
    updated code on 31/10/18

--- A-I.py
+++ A-I.py
@@ -1,3 +1,28 @@
 # This is a version of the A.I that uses rules to choose the best move rather than hard coding
 
-class AI()
+class AI():
+    """This is the class of the A.I that the player will play against. """
+    def __init__(self, opHand, myHand, start):
+        self._opHand = opHand
+        self._myHand = myHand
+        self.start = start
+
+    def _calcOdds(self, state="destroy"):
+        combos = []
+        for myHand in self._myHand:
+            for opHand in self._opHand:
+                if myHand + opHand == 5:
+                    combos.append([self._myHand.index(myHand), self.opHand.index(opHand)])
+        return combos
+
+
+    def attack(self):
+        """This function dictates what to do if the AI decides it wants to attack"""
+        attacking = []
+        if self.start == True:
+            attacking = ['attack', 'left', 'left']
+            return attacking
+
+        else:
+            movesToDestroy = self._calcOdds(state="destroy")
+            if len(movesToDestroy) != 0:
